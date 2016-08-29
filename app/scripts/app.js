4c01a3c0c32874266593a4ba7056c06c10989b34
Daniel Cudney <upnvapesd@gmail.com>
2016-08-29 13:48:31 -0700

    Setting up
    Setting Up folder layout

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+var angular = require('angular');
+angular.module("mainSite");
+//STATE CONFIG
+// require('./scripts/config/main.js');
+//CONTROLLERS
+// require('./scripts/controllers/indexCtrl.js');
+//SERVICE
+// require('./scripts/services/timerService.js');
