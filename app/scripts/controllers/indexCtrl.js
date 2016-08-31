e5197b2e555b86561cd46197fee42f8e15d632ba
Daniel Cudney <upnvapesd@gmail.com>
2016-08-31 11:17:48 -0700

    Menu Update

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -6,7 +6,7 @@ angular.module("mainSite")
       var backgroundResize = function() {
           // document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
           var widthForBG = document.documentElement.clientHeight * 1.669133771929825;
-          document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
+          document.getElementById("backgroundAnimate").style.backgroundSize = widthForBG + "px auto";
         }
         var windowResize = function(object, type, callback) {
           if (object == null || typeof(object) == 'undefined') return;
@@ -23,6 +23,6 @@ angular.module("mainSite")
         var readjust = 0;
         $interval(function () {
           readjust += .1;
-          document.getElementById("bg").style.backgroundPosition = readjust + "px 0px"
+          document.getElementById("backgroundAnimate").style.backgroundPosition = readjust + "px 0px";
         }, 1);
 });
--- public/scripts/mainSite.bundle.js
+++ public/scripts/mainSite.bundle.js
@@ -4626,7 +4626,7 @@ webpackJsonp([0],[
 	      var backgroundResize = function() {
 	          // document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
 	          var widthForBG = document.documentElement.clientHeight * 1.669133771929825;
-	          document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
+	          document.getElementById("backgroundAnimate").style.backgroundSize = widthForBG + "px auto";
 	        }
 	        var windowResize = function(object, type, callback) {
 	          if (object == null || typeof(object) == 'undefined') return;
@@ -4643,7 +4643,7 @@ webpackJsonp([0],[
 	        var readjust = 0;
 	        $interval(function () {
 	          readjust += .1;
-	          document.getElementById("bg").style.backgroundPosition = readjust + "px 0px"
+	          document.getElementById("backgroundAnimate").style.backgroundPosition = readjust + "px 0px";
 	        }, 1);
 	});
 
