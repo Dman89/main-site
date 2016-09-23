ce0cdd4c4c9fd851f420c6bd08e45ddf54b69418
Daniel Cudney <upnvapesd@gmail.com>
2016-09-22 17:14:50 -0700

    Background update

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -7,19 +7,17 @@ angular.module("mainSite")
 
 var docElemOrBody = document.documentElement || document.body;
   // BACKGROUND ANIMATION
+      // var backgroundResize = function() {
+      //   document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
+      //   var docElemOrBody = document.documentElement || document.body;
+      //   var widthForBG = docElemOrBody.clientHeight * 1.669133771929825;
+      //   document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
+      // }
       var backgroundResize = function() {
         document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
-
-
-        var docElemOrBody = document.documentElement || document.body;
-
-
-        var widthForBG = docElemOrBody.clientHeight * 1.669133771929825;
-
-
-        document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
-
-
+        let elem = document.getElementById("bg");
+        let widthForBG = elem.offsetHeight * 1.669133771929825;
+        document.getElementById("bg").style.backgroundSize = "cover";
       }
       var windowResize = function(object, type, callback) {
           if (object == null || typeof(object) == 'undefined') return;
@@ -33,7 +31,7 @@ var docElemOrBody = document.documentElement || document.body;
       };
         backgroundResize();
         windowResize(window, "resize", backgroundResize);
-        var readjust = 0;
+        let readjust = 0;
         $interval(function () {
           readjust += .1;
           document.getElementById("bg").style.backgroundPosition = readjust + "px 0px";
--- public/images/scripts/mainSite.bundle.js
+++ public/images/scripts/mainSite.bundle.js
@@ -4637,17 +4637,19 @@ webpackJsonp([0],[
 
 	var docElemOrBody = document.documentElement || document.body;
 	  // BACKGROUND ANIMATION
-	      // var backgroundResize = function() {
-	      //   document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
-	      //   var docElemOrBody = document.documentElement || document.body;
-	      //   var widthForBG = docElemOrBody.clientHeight * 1.669133771929825;
-	      //   document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
-	      // }
 	      var backgroundResize = function() {
 	        document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
-	        let elem = document.getElementById("bg");
-	        let widthForBG = elem.offsetHeight * 1.669133771929825;
-	        document.getElementById("bg").style.backgroundSize = "cover";
+
+
+	        var docElemOrBody = document.documentElement || document.body;
+
+
+	        var widthForBG = docElemOrBody.clientHeight * 1.669133771929825;
+
+
+	        document.getElementById("bg").style.backgroundSize = widthForBG + "px auto";
+
+
 	      }
 	      var windowResize = function(object, type, callback) {
 	          if (object == null || typeof(object) == 'undefined') return;
@@ -4661,7 +4663,7 @@ webpackJsonp([0],[
 	      };
 	        backgroundResize();
 	        windowResize(window, "resize", backgroundResize);
-	        let readjust = 0;
+	        var readjust = 0;
 	        $interval(function () {
 	          readjust += .1;
 	          document.getElementById("bg").style.backgroundPosition = readjust + "px 0px";
