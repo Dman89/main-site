7e040355f997172705469a45a5ed8b7f121e8a02
Daniel Cudney <upnvapesd@gmail.com>
2016-09-27 22:03:11 -0700

    Fixed Background

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -1,11 +1,9 @@
 'use strict';
 angular.module("mainSite")
 .controller("indexCtrl", function($scope, $interval, $timeout, scrollService, barGraphAnimationService, servicesForSaleService, portfolioService) {
-
+  let docElemOrBody = document.documentElement || document.body;
   setTimeout(function() {window.scrollTo(0, 0)}, 1000)
   setTimeout(function() {window.scrollTo(0, 0)}, 1500)
-
-var docElemOrBody = document.documentElement || document.body;
   // BACKGROUND ANIMATION
       // var backgroundResize = function() {
       //   document.getElementById("bg").style.background = '#212121 url("../../../images/bg.jpg") repeat-x 0 0';
@@ -32,10 +30,13 @@ var docElemOrBody = document.documentElement || document.body;
         backgroundResize();
         windowResize(window, "resize", backgroundResize);
         let readjust = 0;
-        $interval(function () {
-          readjust += .1;
-          document.getElementById("bg").style.backgroundPosition = readjust + "px 0px";
-        }, 1);
+        console.log(docElemOrBody.clientWidth, docElemOrBody.clientHeight);
+        if (docElemOrBody.clientWidth >= 768 && docElemOrBody.clientHeight >= 768) {
+          $interval(function () {
+            readjust += .1;
+            document.getElementById("bg").style.backgroundPosition = readjust + "px 0px";
+          }, 1);
+        }
 
         $scope.portfolio = portfolioService.portfolio;
 });
